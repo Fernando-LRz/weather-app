2b96e024627e8b93086bcb10eec17b1a82e55007
Fernando Lorenzo Ramírez <fernandolorenzo710@gmail.com>
2023-06-13 16:02:39 -0600

    add useWeatherApp hook

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,91 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, ActivityIndicator, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import useCities from '../hooks/useCities';
-import useCurrentWeather from '../hooks/useCurrentWeather';
 import ErrorMessage from '../components/ErrorMessage';
 import SearchCityInput from '../components/SearchCityInput';
 import WeatherInfo from '../components/WeatherInfo';
 import CityOption from '../components/CityOption';
 import Loading from '../components/Loading';
-import onlyLettersAndSpaces from '../helpers/onlyLettersAndSpaces';
 
-import { FullCity, SimpleCity } from '../interfaces/CityInterfaces';
-import { CurrentWeather } from '../interfaces/WeatherInterfaces';
+import useWeatherApp from '../hooks/useWeatherApp';
 
 const HomeScreen = () => {
 
     const [ searchTerm, setSearchTerm ] = useState('');
     const [ onFocus, setOnFocus ] = useState(false);
 
-    const [ cities, setCities ] = useState<SimpleCity[]>([]);
-    const [ isLoadingCities, setIsLoadingCities ] = useState(false);
-
-    const { getCities, getCity } = useCities();
-
-    const [ isAnError, setIsAnError ] = useState(false);
-
-    const [ city, setCity ] = useState<FullCity>({} as FullCity);
-    const [ isLoadingCityInfo, setIsLoadingCityInfo ] = useState(true);
-
-    const [ currentWeather, setCurrentWeather ] = useState<CurrentWeather>({} as CurrentWeather);
-    const [ isLoadingWeather, setIsLoadingWeather ] = useState(true);
-    const { getWeather } = useCurrentWeather();
-
-    const loadCities = async () => {
-        const cities = await getCities(searchTerm);
-        if(!cities || cities.length === 0) return;
-
-        setCities(cities);
-    }
-
-    useEffect(() => {
-        if(cities.length === 0 || !isLoadingCities) return;
-        setIsLoadingCities(false);
-
-    }, [ cities ]);
-
-    const loadDefaultCity = async (id: number = 3453102) => {
-        const cityId = await AsyncStorage.getItem('cityId');
-        if(cityId) id = Number(cityId);
-
-        const city = await getCity(id);
-        setIsLoadingCityInfo(false);
-
-        if(!city) {
-            setIsAnError(true);
-            return;
-        }
-        
-        setCity(city.data);
-    }
-
-    const loadNewCity = async (id: number) => {
-        setIsLoadingCityInfo(true);
-        setIsLoadingWeather(true);
-
-        await AsyncStorage.setItem('cityId', id.toString());
-
-        loadDefaultCity();
-    }
-
-    const loadDefaultCityCurrentWeather = async (lat: number, lon: number) => {
-        const weather = await getWeather(lat, lon);
-        setIsLoadingWeather(false);
-
-        if(!weather) {
-            setIsAnError(true);
-            return;
-        }
-
-        const iconUri = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
-
-        weather.main.description = weather.weather[0].description;
-        weather.main.icon = iconUri;
-        
-        setCurrentWeather(weather.main);
-    }
+    const { 
+            city, 
+            cities, 
+            loadDefaultCity, 
+            loadDefaultCityCurrentWeather,
+            isLoadingCityInfo, 
+            isLoadingWeather,
+            isLoadingCities,
+            currentWeather, 
+            isAnError, 
+            loadNewCity,
+            loadCities
+    } = useWeatherApp();
+    
 
     useEffect(() => {
         loadDefaultCity();
@@ -97,13 +39,7 @@ const HomeScreen = () => {
     }, [ city ])
 
     useEffect(() => {
-        setCities([]);
-   
-        if( !searchTerm || !onlyLettersAndSpaces(searchTerm)) return;
-
-        setIsLoadingCities(true);
-        loadCities();
-
+        loadCities(searchTerm);
     }, [ searchTerm ]);
 
     if(isLoadingCityInfo && !isAnError || isLoadingWeather && !isAnError ) {
