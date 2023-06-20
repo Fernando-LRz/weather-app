85e5d4b14ae63830eee9d45b6ed3cf35a7c03438
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-06-19 21:32:34 -0600

    removing api keys after deactivation

--- src/api/GeoDBApi.tsx
+++ src/api/GeoDBApi.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const GeoDBApi = axios.create({
     baseURL: 'https://wft-geo-db.p.rapidapi.com/v1/geo',
     headers: {
-        'X-RapidAPI-Key': '098acf6c32msh6eba01cc073d67cp1247aejsn8fc160048fbf',
+        'X-RapidAPI-Key': 'API_KEY_HERE',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 });
--- src/api/OpenWeatherApi.tsx
+++ src/api/OpenWeatherApi.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const OpenWeatherApi = axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5',
     params: {
-        appid: '394d380d13100ca249790f5342d7e519'
+        appid: 'API_KEY_HERE'
     }
 });
 
